4eb8791203c9fdc1dc3040bb1cc0d1f106463bb9
Bernhard Keprt <bernhard@appointmed.com>
2021-12-06 10:36:37 +0100

    remove sns test topic

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,6 +1,5 @@
-import {DockerImage, Stack} from 'aws-cdk-lib';
+import {Stack} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {Topic} from 'aws-cdk-lib/aws-sns';
 import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
 import {Runtime} from 'aws-cdk-lib/aws-lambda';
 
@@ -10,10 +9,6 @@ export class ServiceStack extends Stack {
                 id: string,
                 properties: any) {
         super(scope, id, properties);
-
-        new Topic(this, 'cdkv2topic', {
-            topicName: 'cdk-v2-test-topic'
-        });
 
         new NodejsFunction(this, 'TestFunction', {
             runtime: Runtime.NODEJS_14_X,
@@ -38,7 +33,6 @@ export class ServiceStack extends Stack {
                         return [
                             // 'npm update -g npm', //<<- this leads to error when building locally
                             'cd ./asset-input/',
-                            'npm --version',
                             'echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc '
                         ];
                     },
